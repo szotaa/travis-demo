5f8ad075d623e2c8b008c10a6bba2e6ee31a8fd9
szotaa <kubaszota97@gmail.com>
2018-06-15 17:58:35 +0200

    initial spring boot app

---
+++ src/main/java/pl/szotaa/travisdemo/TravisDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.szotaa.travisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisDemoApplication.class, args);
+    }
+}
